1ef95cba558d2423ea3cd2ea28af7744b9f98d8b
yurii <jurakulek@gmail.com>
2021-02-28 20:04:38 +0200

    Removed redudant signal from celery

--- breaking_brain_api/celery.py
+++ breaking_brain_api/celery.py
@@ -6,12 +6,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'breaking_brain_api.settings')
 
-
-@celery.signals.setup_logging.connect
-def on_celery_setup_logging(**kwargs):
-    pass
-
-
 app = Celery('breaking_brain_api')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
