17979e99f3b773e8e51f10145148ac8bb194a3e5
Yurii Kulyk <jurakulek@gmail.com>
2021-05-08 22:24:39 +0300

    Added questions action

--- quizzes/tests.py
+++ quizzes/tests.py
@@ -12,6 +12,7 @@ class QuizTest(BaseAPITest):
         self.question = mixer.blend(Question, quiz=self.quiz)
         self.option = mixer.blend(Option, question=self.question)
         self.lesson = mixer.blend(Lesson, quiz=self.quiz)
+        self.user = self.create_and_login()
 
     def test_get_list_of_quizes(self):
         resp = self.client.get(reverse('quizzes-list'))
@@ -22,6 +23,11 @@ class QuizTest(BaseAPITest):
         self.assertEqual(resp.data['results'][0]['lessons'][0]['id'], self.lesson.id)
         self.assertEqual(resp.data['results'][0]['lessons'][0]['title'], self.lesson.title)
 
+    def test_get_list_of_quizes_when_logged_out(self):
+        self.logout()
+        resp = self.client.get(reverse('quizzes-list'))
+        self.assertEqual(resp.status_code, 200)
+
     def test_get_single_quiz(self):
         resp = self.client.get(reverse('quizzes-detail', args=(self.quiz.id,)))
         self.assertEqual(resp.status_code, 200)
@@ -30,6 +36,27 @@ class QuizTest(BaseAPITest):
         self.assertEqual(resp.data['lessons'][0]['id'],
                          self.lesson.id)
 
+    def test_get_single_quiz_when_logout(self):
+        self.logout()
+        resp = self.client.get(reverse('quizzes-detail', args=(self.quiz.id,)))
+        self.assertEqual(resp.status_code, 200)
+
     def test_get_non_existed_quiz(self):
         resp = self.client.get(reverse('quizzes-detail', args=(123123,)))
         self.assertEqual(resp.status_code, 404)
+    
+    def test_get_questions_from_quiz(self):
+        resp = self.client.get(reverse('quizzes-questions', args=(self.quiz.id,)))
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data[0]['id'], self.question.id)
+        self.assertEqual(resp.data[0]['options'][0]['id'], self.option.id)
+        self.assertEqual(len(resp.data), 1)
+    
+    def test_get_quesionts_from_non_existed_quiz(self):
+        resp = self.client.get(reverse('quizzes-questions', args=(12311,)))
+        self.assertEqual(resp.status_code, 404)
+
+    def test_get_questions_from_quiz_when_logout(self):
+        self.logout()
+        resp = self.client.get(reverse('quizzes-questions', args=(self.quiz.id,)))
+        self.assertEqual(resp.status_code, 401)
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,9 +1,12 @@
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import action
 
 from breaking_brain_api.paginators import ResultSetPagination
 from quizzes.models import Quiz
-from quizzes.serializers import QuizSerializer
+from quizzes.serializers import QuestionSerializer, QuizSerializer
 
 
 class QuizViewSet(ReadOnlyModelViewSet):
@@ -17,6 +20,11 @@ class QuizViewSet(ReadOnlyModelViewSet):
     Return one quiz by its id
 
     Return one quiz by its id
+
+    questions:
+    Get list of questions for single quiz
+    
+    Get list of questions for single quiz
     """
 
     serializer_class = QuizSerializer
@@ -25,4 +33,10 @@ class QuizViewSet(ReadOnlyModelViewSet):
 
 
     def get_queryset(self):
-        return Quiz.objects.all().prefetch_related('tags', 'questions__options')
+        return Quiz.objects.all().prefetch_related('tags', 'lessons')
+
+    @action(detail=True, methods=['GET'], serializer_class=QuestionSerializer,
+            permission_classes=(IsAuthenticated,))
+    def questions(self, request, pk=None, *args, **kwargs):
+        quiz = get_object_or_404(Quiz, pk=pk)
+        return Response(data=self.serializer_class(quiz.questions.all(), many=True).data)
